e9306de988a91f4f3b7648b47c52f7d095c9e26a
dimabille-hue <dimabille@gmail.com>
2026-07-16 15:44:37 +0700

    Create parser.py

---
+++ etl/egrn/parser.py
@@ -0,0 +1,23 @@
+FIELD_MAP = {
+
+    "cad_number":
+        ".//CadastralNumber",
+
+    "area":
+        ".//Area",
+
+    "address":
+        ".//Address"
+}
+class XMLParser(Parser):
+
+    FIELD_MAP = {
+
+        "cad_number": ".//CadastralNumber",
+
+        "area": ".//Area",
+
+        "address": ".//Address"
+    }
+
+    ...
