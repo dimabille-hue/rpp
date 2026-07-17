ecbeffe23ec3dea2947a092c344e293a38e23fcf
dimabille-hue <dimabille@gmail.com>
2026-07-17 08:45:53 +0700

    Create statistcs.py

---
+++ source/comparable/statistcs.py
@@ -0,0 +1,24 @@
+import statistics
+
+
+class ComparableStatistics:
+
+    def median(self, rows):
+
+        ...
+
+    def average(self, rows):
+
+        ...
+
+    def std(self, rows):
+
+        ...
+
+    def coefficient_of_variation(self, rows):
+
+        ...
+
+    def iqr(self, rows):
+
+        ...
